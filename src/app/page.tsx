34b1cd72266ff9c4af6a82976dd7638155dd3160
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-05-07 12:37:14 -0500

    Update globals.css and page.tsx with shadow styles and layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-"use client"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 import NavBar from "@/components/NavBar"
 
@@ -17,7 +16,7 @@ const Home = () => {
       <NavBar />
 
 
-      <section className="text-white animate-duration-slower animate-fade-in-right p-4 h-[80dvh] flex flex-col mx-2  z-10 justify-center items-center">
+      <section className="text-white animate-duration-slower animate-fade-in-right p-4 h-[250px] flex flex-col mx-2  z-10 justify-end items-center">
         <div className="font-bebas text-start">
           <h2 className="text-3xl text-gray-400">Jonas Rosales</h2>
           <span className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300 dark:before:border-neutral-500 dark:after:border-neutral-500" />
@@ -26,6 +25,21 @@ const Home = () => {
             <p className="text-4xl">➕ Web Development Enthusiast</p>
           </div>
         </div>
+      </section>
+
+      <section className="w-[90dvw] mx-[5dvw] bg-transparent h-[50dvh] flex justify-between items-end opacity-80 animate-slide-in-top">
+        <div className="w-1 h-[30dvh] bg-purple-600 shadowPurple" />
+        <div className="w-1 bg-green-500 h-[40dvh] shadowGreen" />
+        <div className="w-1 bg-pink-700 h-[20dvh] shadowPink" />
+        <div className="w-1 bg-blue-500 h-[10dvh] shadowBlue" />
+        <div className="w-1 bg-blue-500 h-[10dvh] shadowBlue" />
+        <div className="w-1 bg-pink-700 h-[20dvh] shadowPink" />
+        <div className="w-1 bg-green-500 h-[40dvh] shadowGreen" />
+        <div className="w-1 h-[30dvh] bg-purple-600 shadowPurple" />
+      </section>
+
+      <section className="bg-slate-950 w-full h-[400px]">
+
       </section>
 
 
