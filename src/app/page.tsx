0a11cc3e1e47b1690edbb18713bd83c2c60f1f7d
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-05-07 19:56:05 -0500

    Adjust shadow styles and section heights in globals.css and NavBar.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,19 +10,34 @@ const Home = () => {
   const [greenClassName, setGreenClassName] = useState("w-1 bg-green-500 h-[45dvh] shadowGreen lg:h-[50dvh]")
   const [pinkClassName, setPinkClassName] = useState("w-1 bg-pink-700 h-[25dvh] shadowPink lg:h-[30dvh]")
   const [blueClassName, setBlueClassName] = useState("w-1 bg-blue-500 h-[15dvh] shadowBlue lg:h-[20dvh]")
+  const [textBlueClassName, setTextBlueClassName] = useState("text-3xl text-blue-500 textShadow")
+  const [textGreenClassName, setTextGreenClassName] = useState("text-3xl text-green-500 textShadow")
+  const [textPurple, setTextPurple] = useState("text-4xl pt-1 text-purple-700 textShadow")
+  const [imgClassName, setImgClassName] = useState("w-40 md:w-52 shadowImg")
+  const [iconClassName, setIconClassName] = useState("w-10 h-10 iconShadow")
 
   useEffect(() => {
     setInterval(() => {
-      setPurpleClassName("w-1 h-[35dvh] bg-purple-600 shadowPurple lg:h-[40dvh] shadowPurpleBright")
-      setGreenClassName("w-1 bg-green-500 h-[45dvh] shadowGreen lg:h-[50dvh] shadowGreenBright")
-      setPinkClassName("w-1 bg-pink-700 h-[25dvh] shadowPink lg:h-[30dvh] shadowPinkBright")
-      setBlueClassName("w-1 bg-blue-500 h-[15dvh] shadowBlue lg:h-[20dvh] shadowBlueBright")
+      setPurpleClassName("w-1 h-[35dvh] bg-purple-600 shadowPurple lg:h-[40dvh] shadowPurpleBright md:w-3")
+      setGreenClassName("w-1 bg-green-500 h-[45dvh] shadowGreen lg:h-[50dvh] shadowGreenBright md:w-3")
+      setPinkClassName("w-1 bg-pink-700 h-[25dvh] shadowPink lg:h-[30dvh] shadowPinkBright md:w-3")
+      setBlueClassName("w-1 bg-blue-500 h-[15dvh] shadowBlue lg:h-[20dvh] shadowBlueBright md:w-3")
+      setTextBlueClassName("text-3xl text-blue-500 textShadowBright")
+      setTextGreenClassName("text-3xl text-green-500 textShadowBright")
+      setTextPurple("text-4xl pt-1 text-purple-700 textShadowBright")
+      setImgClassName("w-40 md:w-52 shadowImgBright")
+      setIconClassName("w-10 h-10 iconShadowBright")
     }, 2000)
     setInterval(() => {
-      setPurpleClassName("w-1 h-[35dvh] bg-purple-600 shadowPurple lg:h-[40dvh]")
-      setGreenClassName("w-1 bg-green-500 h-[45dvh] shadowGreen lg:h-[50dvh]")
-      setPinkClassName("w-1 bg-pink-700 h-[25dvh] shadowPink lg:h-[30dvh]")
-      setBlueClassName("w-1 bg-blue-500 h-[15dvh] shadowBlue lg:h-[20dvh]")
+      setPurpleClassName("w-1 h-[35dvh] bg-purple-600 shadowPurple lg:h-[40dvh] md:w-3")
+      setGreenClassName("w-1 bg-green-500 h-[45dvh] shadowGreen lg:h-[50dvh] md:w-3")
+      setPinkClassName("w-1 bg-pink-700 h-[25dvh] shadowPink lg:h-[30dvh] md:w-3")
+      setBlueClassName("w-1 bg-blue-500 h-[15dvh] shadowBlue lg:h-[20dvh] md:w-3")
+      setTextBlueClassName("text-3xl text-blue-500 textShadow")
+      setTextGreenClassName("text-3xl text-green-500 textShadow")
+      setTextPurple("text-4xl pt-1 text-purple-700 textShadow")
+      setImgClassName("w-40 md:w-52 shadowImg")
+      setIconClassName("w-10 h-10 iconShadow")
     }, 4000)
   }, [])
 
@@ -31,24 +46,24 @@ const Home = () => {
     <>
       <NavBar />
 
-      <section className="mt-20 lg:mt-10 mb-10 h-[40vh] flex flex-col items-center justify-center md:flex-row gap-4 md:items-center">
+      <section className="mt-20 lg:mt-10 mb-10 h-[40vh] flex flex-col items-center justify-center md:flex-row gap-4 md:items-center md:mb-0">
         <aside className="md:order-1">
-          <img className="w-52" src="/computadora.png" />
+          <img className={imgClassName} src="/computadora.png" />
         </aside>
         <div className="font-bebas flex flex-col animate-duration-slower animate-fade-in-right p-4  mx-2 justify-end items-center">
           <div>
-            <h2 className="text-3xl text-blue-500 textShadow">Jonas Rosales</h2>
+            <h2 className={textBlueClassName}>Jonas Rosales</h2>
             <span className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300 dark:before:border-neutral-500 dark:after:border-neutral-500" />
-            <p className="text-4xl text-green-600 textShadow">Software Engineering Student</p>
+            <p className={textGreenClassName}>Software Engineering Student</p>
             <div className="flex py-2 gap-3 items-center">
-              <img className="w-10 h-10 shadowImg" src="/plus.png" />
-              <p className="text-4xl pt-1 text-purple-700 textShadow">Web Development Enthusiast</p>
+              <img className={iconClassName} src="/plus.png" />
+              <p className={textPurple}>Web Development Enthusiast</p>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="mx-6 bg-transparent h-[50dvh] flex justify-between items-end animate-slide-in-top">
+      <section className="mx-6 h-[50dvh] flex justify-between items-end animate-slide-in-top">
         <div className={purpleClassName} />
         <div className={greenClassName} />
         <div className={pinkClassName} />
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -27,13 +27,13 @@ const NavBar: React.FC = () => {
       <div className="fixed top-0 left-0 w-screen h-screen backdrop-blur-md bg-indigo-950/60  md:bg-transparent flex justify-center items-center inset-0 translate-x-full peer-checked:translate-x-0 transition-transform md:static  md:translate-x-0 z-30 md:hidden">
         <div className="lg:mr-28">
           <ul className="tracking-wide backdrop-blur-lg md:outline-none absolute inset-x-0 top-24 p-12 w-[90%] mx-auto rounded-lg h-max text-center grid gap-10 md:w-max md:bg-transparent md:p-0 md:grid-flow-col md:static">
-            <li className="hover:scale-110 ease-in-out duration-200 hover:text-purple-500">
+            <li className="hover:scale-110 hoverText hoverTextBlue">
               <Link href='/LogIn'>Projects</Link>
             </li>
-            <li className="hover:scale-110 ease-in-out duration-200 hover:text-purple-500">
+            <li className="hover:scale-110 hoverText hoverTextGreen">
               <Link href='/Contact'>Skills</Link>
             </li>
-            <li className="hover:scale-110 ease-in-out duration-200 hover:text-purple-500">
+            <li className="hover:scale-110 hoverText hoverTextPink">
               <Link href='/About'>About</Link>
             </li>
           </ul>
@@ -41,13 +41,13 @@ const NavBar: React.FC = () => {
       </div>
 
       <ul className="tracking-wide hidden backdrop-blur-lg  h-max md:grid gap-10 w-max md:bg-transparent md:grid-flow-col">
-        <li className="hover:scale-110 ease-in-out duration-200 hover:text-purple-500">
+        <li className="hover:scale-110 hoverText hoverTextBlue">
           <Link href='/LogIn'>Projects</Link>
         </li>
-        <li className="hover:scale-110 ease-in-out duration-200 hover:text-purple-500">
+        <li className="hover:scale-110 hoverText hoverTextGreen">
           <Link href='/Contact'>Skills</Link>
         </li>
-        <li className="hover:scale-110 ease-in-out duration-200 hover:text-purple-500">
+        <li className="hover:scale-110 hoverText hoverTextPink">
           <Link href='/About'>About</Link>
         </li>
       </ul>
