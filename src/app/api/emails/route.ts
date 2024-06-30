97322dd9b653ce791cd3aff3e703ba677466583f
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-06-30 18:26:03 -0500

    chore: Refactor email sending logic
    This commit refactors the email sending logic in the POST function of the emails route. It removes the unnecessary assignment of the result variable and improves the error handling by logging any errors that occur during the sending process. This change enhances the reliability and maintainability of the code.

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: NextRequest) {
 	}
 
 	try {
-		const result = await transporter.sendMail(mailOptions)
+		await transporter.sendMail(mailOptions)
 	} catch (error) {
 		console.log(error)
 	}
